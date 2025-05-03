9bcf7850e8c70b507d77cbc000e474c97194dbb3
JayYarlagadda <jayanthyarlagadda@gmail.com>
2025-05-03 12:00:00 -0700

    Creating_Reservation_details

---
+++ src/pages/restaurant/RestaurantForm.js
@@ -0,0 +1,8 @@
+// src/pages/restaurant/Dashboard.js
+import React from 'react';
+
+const RestaurantForm = () => {
+  return <div>Restaurant Form (Coming in Week 4)</div>;
+};
+
+export default RestaurantForm;
