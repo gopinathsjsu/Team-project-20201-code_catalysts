0b5e5c2da0a6491ff1405d6720ff42ce4ae0e815
JayYarlagadda <jayanthyarlagadda@gmail.com>
2025-05-03 12:00:00 -0700

    Creating_Reservation_details

---
+++ src/pages/restaurant/ReservationDetail.js
@@ -0,0 +1,8 @@
+// src/pages/restaurant/ReservationDetail.js
+import React from 'react';
+
+const ReservationDetail = () => {
+  return <div>Reservation Detail (Coming in Week 4)</div>;
+};
+
+export default ReservationDetail;
